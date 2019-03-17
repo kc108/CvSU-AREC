e1f11ad48a5e153e6e1dec082c79300cc94f2daf
Rhalp Darren R. Cabrera <rhalpdarrencabrera@gmail.com>
2019-03-18 02:54:39 +0800

    dash-mapper

--- dashboard/mapper.php
+++ dashboard/mapper.php
@@ -1,11 +1,11 @@
-﻿ <?php 
+﻿<?php 
     include('../session.php');
     include('dash-global-function.php');
 
    
     $pagename = "Biogas Mapper";
     $username = $_SESSION['user_Name'];
-    $script_for_specific_page = "jquery";
+    $script_for_specific_page = "";
     $user_img = "../assets/images/user.png";
     $user_email = "mail@gmail.com";
     if(isset($_SESSION['login_level']) )
@@ -28,10 +28,12 @@
 
 <!DOCTYPE html>
 <html>
+
  <?php
     include("dash-head.php");
     ?>
-<body class="theme-green ls-closed">
+
+<body class="theme-green ">
     <!-- Page Loader -->
     <div class="page-loader-wrapper">
         <div class="loader">
@@ -67,83 +69,257 @@
             <div class="block-header">
                 <h2>
                     Biogas Data Mapper
-                    
                 </h2>
             </div>
-            
-             <!-- Basic Examples -->
+
+            <ol class="breadcrumb breadcrumb-bg-green">
+                <li><a href="index"><i class="material-icons">home</i> Home</a></li>
+                <li  class="active"><a href="javascript:void(0);"><i class="material-icons ">map</i> Biogas Mapper</a></li>
+            </ol>
             <div class="row clearfix">
-                <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
-                    <div class="card">
-                        <h2>List of Biogas</h2>
-                        <table  class="table table-bordered">
-                            <thead>
-                                <tr>
-                                    <th>Name</th>
-                                    <th>Visible</th>
-                                    <th>Option</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <?php 
-                                   $sql =  mysqli_query($conn,"SELECT * FROM `locations`");
-                                   while ($row = mysqli_fetch_array($sql)) {
-                                    ?>
-                                    <tr>
-                                        <td><?php echo $row['title']?></td>
-                                        <td><?php 
-                                        if ($row['location_status'] == 0) {
-                                            echo "Hidden";
-                                        }
-                                        else{
-                                            echo "Visible";
-                                        }
-                                         ?></td>
-                                        <td>
-                                            <div class="btn-group">
-                                              <button type="button" class="btn btn-primary">Action</button>
-                                              <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown">
-                                                <span class="caret"></span>
-                                              </button>
-                                              <ul class="dropdown-menu" role="menu">
-                                                <li><a href="#">Hide</a></li>
-                                                <?php 
-                                                if ($row['location_status'] == 0) {
-                                                    echo '<li><a href="#">Hide</a></li>';
-                                                }
-                                                else{
-                                                    echo '<li><a href="#">Show</a></li>';
-                                                }
-                                                ?>
-                                                <li><a href="#">Update</a></li>
-                                                <li><a href="#">Delete</a></li>
-                                              </ul>
-                                            </div>
-                                        </td>
-                                    </tr>
-                                    <?php
-                                   }
-                                ?>
-                                
-                            </tbody>
-                        </table>
+                       <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                           <div class="card">
+                               <div class="header">
+                                   <h2>LIST OF BIOGAS</h2>
+                                   <div class="btn-group pull-right">
+                                   <button type="button" class="btn btn-info waves-effect" data-toggle="modal" data-target="#largeModal">BIOGAS MAPPER</button>
+                                   <button type="button" class="btn btn-info waves-effect reload_table">RELOAD TABLE</button>
+                                   </div>
+                                   <br>
+                               </div>
+                               <div class="body">
+                                   <div class="table-responsive" style="overflow-x: hidden;">
+                                       <table id="biogas_data" class="table table-bordered table-striped">
+                                           <thead>
+                                               <tr>
+                                                   <th width="10%">ID</th>
+                                                   <th width="35%">Title</th>
+                                                   <th width="35%">Mark Visibility</th>
+                                                   <th width="10%">Action</th>
+                                               </tr>
+                                           </thead>
+                                       </table>
+                                       
+                                   </div>
+                               </div>
+                           </div>
                     </div>
-                </div>
-                <div class="col-lg-8 col-md-8 col-sm-8 col-xs-8">
+            </div>   
+          <!--    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
                     <div class="card">
                         <iframe src="map/user-map.php" style=" display:block; width:100%; height: 800px;"></iframe>
                     </div>
-                </div>
-            </div>
-            <!-- #END# Basic Examples -->
-            
+                </div> -->
+          
         </div>
+
     </section>
 
-    <?php 
-        include("dash-js.php");
-    ?>
+<!-- Large Size -->
+<div class="modal fade" id="largeModal" tabindex="-1" role="dialog">
+    <div class="modal-dialog modal-lg" role="document">
+        <div class="modal-content">
+            <div class="modal-header bg-cyan">
+                <h4 class="modal-title" id="largeModalLabel">MAP</h4>
+            </div>
+            <div class="modal-body">
+                <i>Note: Right Click For Mark and Left Click For Remove </i> 
+               <iframe src="map/user-map.php" style=" display:block; width:100%; height: 800px;"></iframe>
+            </div>
+            <div class="modal-footer " style="background-color: #e4e4e4;">
+                <button type="button" class="btn btn-link btn-danger waves-effect" data-dismiss="modal" style=" color: white;">CLOSE</button>
+            </div>
+        </div>
+    </div>
+</div>
+<div id="biogas_modal" class="modal fade">
+    <div class="modal-dialog">
+        <form method="post" id="biogas_form" enctype="multipart/form-data" class="form-horizontal">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                    <h4 class="modal-title list_biogas">Modal title</h4>
+                </div>
+                <div class="modal-body">
+                    <div class="row clearfix">
+                        <div class="col-lg-2 col-md-2 col-sm-4 col-xs-5 form-control-label">
+                            <label for="loc_title">Title</label>
+                        </div>
+                        <div class="col-lg-10 col-md-10 col-sm-8 col-xs-7">
+                            <div class="form-group">
+                                 <input type="text" name="loc_title" id="loc_title" class="form-control" />
+                            </div>
+                    </div>
+                    <br>
+                    <div class="row clearfix">
+                        <div class="col-lg-2 col-md-2 col-sm-4 col-xs-5 form-control-label">
+                            <label for="loc_descr">Description</label>
+                        </div>
+                        <div class="col-lg-10 col-md-10 col-sm-8 col-xs-7">
+                            <div class="form-group">
+                                <textarea name="loc_descr" id="loc_descr" class="form-control"></textarea>
+                                </div>
+                            </div>
+                    </div>
+                    <br>
+                    <div class="row clearfix">
+                        <div class="col-lg-2 col-md-2 col-sm-4 col-xs-5 form-control-label">
+                            <label for="loc_stat">Active</label>
+                        </div>
+                        <div class="col-lg-10 col-md-10 col-sm-8 col-xs-7">
+                            <div class="form-group">
+                                <div class="form-line">
+                                     <select class="form-control" name="loc_stat" id="loc_stat" >
+                                      <option value="null">~~SELECT~~</option>
+                                        <option value="1">Show</option>
+                                        <option value="0">Hide</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+                    </div> 
+                </div>
+                <div class="modal-footer">
+                    <input type="hidden" name="location_id" id="location_id" />
+                    <input type="hidden" name="operation" id="operation" />
+                    <input type="submit" name="action" id="action" class="btn btn-success" value="Edit" />
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                </div>
+            </div>
+        </form>
+    </div>
+</div>
+    <!-- Jquery Core Js -->
+    <script src="../assets/plugins/jquery/jquery.min.js"></script>
 
+    <!-- Bootstrap Core Js -->
+    <script src="../assets/plugins/bootstrap/js/bootstrap.js"></script>
+
+    <!-- Select Plugin Js -->
+    <script src="../assets/plugins/bootstrap-select/js/bootstrap-select.js"></script>
+
+    <!-- Slimscroll Plugin Js -->
+    <script src="../assets/plugins/jquery-slimscroll/jquery.slimscroll.js"></script>
+
+    <!-- Waves Effect Plugin Js -->
+    <script src="../assets/plugins/node-waves/waves.js"></script>
+
+    <!-- Jquery DataTable Plugin Js -->
+    <script src="../assets/plugins/jquery-datatable/jquery.dataTables.js"></script>
+    <script src="../assets/plugins/jquery-datatable/skin/bootstrap/js/dataTables.bootstrap.js"></script>
+    <script src="../assets/plugins/jquery-datatable/extensions/export/dataTables.buttons.min.js"></script>
+    <script src="../assets/plugins/jquery-datatable/extensions/export/buttons.flash.min.js"></script>
+    <script src="../assets/plugins/jquery-datatable/extensions/export/jszip.min.js"></script>
+    <script src="../assets/plugins/jquery-datatable/extensions/export/pdfmake.min.js"></script>
+    <script src="../assets/plugins/jquery-datatable/extensions/export/vfs_fonts.js"></script>
+    <script src="../assets/plugins/jquery-datatable/extensions/export/buttons.html5.min.js"></script>
+    <script src="../assets/plugins/jquery-datatable/extensions/export/buttons.print.min.js"></script>
+
+    <!-- Custom Js -->
+    <script src="../assets/js/admin.js"></script>
+    <script src="../assets/js/pages/tables/jquery-datatable.js"></script>
+
+    <!-- Demo Js -->
+    <script src="../assets/js/demo.js"></script>
+    <script type="text/javascript" language="javascript" >
+$(document).ready(function(){
+
+
+
+    var dataTable = $('#biogas_data').DataTable({
+        "processing":true,
+        "serverSide":true,
+        "order":[],
+        "ajax":{
+            url:"datatable/biogas/fetch.php",
+            type:"POST"
+        },
+        "columnDefs":[
+            {
+                "targets":[0],
+                "orderable":false,
+            },
+        ],
+
+    });
+    $(document).on('submit', '#biogas_form', function(event){
+        event.preventDefault();
+        var loc_title = $('#loc_title').val();
+        var loc_descr = $('#loc_descr').val();
+        
+        if(loc_title != '' && loc_descr != '')
+        {
+            $.ajax({
+                url:"datatable/biogas/insert.php",
+                method:'POST',
+                data:new FormData(this),
+                contentType:false,
+                processData:false,
+                success:function(data)
+                {
+                    alert(data);
+                    $('#biogas_form')[0].reset();
+                    $('#biogas_modal').modal('hide');
+                    dataTable.ajax.reload();
+                }
+            });
+        }
+        else
+        {
+            alert("Both Fields are Required");
+        }
+    });
+
+    
+
+    $(document).on('click', '.update', function(){
+        var location_id = $(this).attr("id");
+        $.ajax({
+            url:"datatable/biogas/fetch_single.php",
+            method:"POST",
+            data:{location_id:location_id},
+            dataType:"json",
+            success:function(data)
+            {
+                $('#biogas_modal').modal('show');
+                $('#loc_title').val(data.loc_title);
+                $('#loc_descr').val(data.loc_descr);
+                $('.list_biogas').text("Edit Biogas Info");
+                $('#location_id').val(location_id);
+                $('#action').val("Edit");
+                $('#operation').val("Edit");
+            }
+        })
+    });
+    
+    $(document).on('click', '.delete', function(){
+        var location_id = $(this).attr("id");
+        if(confirm("Are you sure you want to delete this?"))
+        {
+            $.ajax({
+                url:"datatable/biogas/delete.php",
+                method:"POST",
+                data:{location_id:location_id},
+                success:function(data)
+                {
+                    alert(data);
+                    dataTable.ajax.reload();
+                }
+            });
+        }
+        else
+        {
+            return false;   
+        }
+    });
+    
+    $(document).on('click', '.reload_table', function(){
+        dataTable.ajax.reload();
+    });
+    
+    
+});
+</script>
 </body>
 
 </html>
