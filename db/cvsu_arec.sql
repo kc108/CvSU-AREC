86eb179cc1410dab8beea70370639d87ae44ef35
Rhalp Darren R. Cabrera <rhalpdarrencabrera@gmail.com>
2019-03-21 04:45:23 +0800

    database

--- db/cvsu_arec.sql
+++ db/cvsu_arec.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 17, 2019 at 05:58 PM
+-- Generation Time: Mar 20, 2019 at 09:36 PM
 -- Server version: 10.1.26-MariaDB
 -- PHP Version: 7.1.8
 
@@ -46,7 +46,35 @@ INSERT INTO `locations` (`id`, `lat`, `lng`, `title`, `description`, `location_s
 (11, 14.896259, 121.116386, 'Darren', '123123123123', 0),
 (13, 14.628011, 120.753838, 'asdasd', 'asdasd', 1),
 (14, 14.683812, 120.649467, 'new32', 'ew32', 1),
-(15, 14.550928, 120.893913, 'asdasd', 'asdasd', 1);
+(15, 14.550928, 120.893913, '123123123', '3123123', 1),
+(16, 13.282024, 121.280090, 'biogas21', 'asdasd', 1),
+(17, 14.196854, 121.007919, 'aaaaaaaaaaaaaa', 'aaaaaaaaaaaaaaa', 1),
+(18, 14.449292, 120.680290, 'asdasd', 'asdasdasd', 1),
+(19, 14.364165, 120.924736, 'xxx', 'xxxx', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `research`
+--
+
+CREATE TABLE `research` (
+  `research_ID` int(11) UNSIGNED NOT NULL,
+  `research_Title` varchar(85) NOT NULL,
+  `research_Content` longtext NOT NULL,
+  `research_Status` tinyint(4) NOT NULL,
+  `research_Created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `research_Attachment` varchar(85) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `research`
+--
+
+INSERT INTO `research` (`research_ID`, `research_Title`, `research_Content`, `research_Status`, `research_Created`, `research_Attachment`) VALUES
+(1, 'research1', 'content content sample sample content content sample sample content content sample sample content content sample sample content content sample sample content content sample sample content content sample sample content content sample sample content content sample sample content content sample sample content content sample sample content content sample sample content content sample sample content content sample sample content content sample sample content content sample sample content content sample sample content content sample sample content content sample sample content content sample sample content content sample sample content content sample sample content content sample sample content content sample sample content content sample sample content content sample sample content content sample sample content content sample sample content content sample sample ', 0, '2019-03-20 19:14:26', ''),
+(2, 'research2', 'content content sample sample content content sample sample content content sample sample content content sample sample content content sample sample content content sample sample content content sample sample content content sample sample content content sample sample content content sample sample content content sample sample content content sample sample content content sample sample content content sample sample content content sample sample content content sample sample content content sample sample content content sample sample content content sample sample content content sample sample content content sample sample content content sample sample content content sample sample content content sample sample content content sample sample content content sample sample content content sample sample content content sample sample content content sample sample ', 0, '2019-03-20 19:14:26', ''),
+(3, 'research3', 'content content sample sample content content sample sample content content sample sample content content sample sample content content sample sample content content sample sample content content sample sample content content sample sample content content sample sample content content sample sample content content sample sample content content sample sample content content sample sample content content sample sample content content sample sample content content sample sample content content sample sample content content sample sample content content sample sample content content sample sample content content sample sample content content sample sample content content sample sample content content sample sample content content sample sample content content sample sample content content sample sample content content sample sample content content sample sample ', 0, '2019-03-20 19:14:26', '');
 
 -- --------------------------------------------------------
 
@@ -80,7 +108,7 @@ INSERT INTO `user_accounts` (`user_ID`, `level_ID`, `user_Name`, `user_Pass`, `u
 (10, 3, '12ui3yusad', 'QrUgcNdRjaE74hfEIeThKa/RaqA9N/KpBI+X7VeiyfE=', '123123@gmail.com', '2019-03-12 13:50:57', 2),
 (12, 3, '321312', 'QrUgcNdRjaE74hfEIeThKa/RaqA9N/KpBI+X7VeiyfE=', 'rhalpdarrencabrera@gmail.com', '2019-03-12 14:42:28', 0),
 (13, 3, 'raizen21', 'QrUgcNdRjaE74hfEIeThKa/RaqA9N/KpBI+X7VeiyfE=', 'rhalpdarrencabrera@gmail.com', '2019-03-12 14:46:04', 1),
-(14, 3, 'raizen27', 'QrUgcNdRjaE74hfEIeThKa/RaqA9N/KpBI+X7VeiyfE=', 'rhalpdarrencabrera@gmail.com', '2019-03-12 14:46:59', 0);
+(14, 3, 'raizen27', 'PVKibfXGxoOxc+WlrZLFCQXErLOsHLCB8F9DsNFD+4s=', 'rhalpdarrencabrera@gmail.com', '2019-03-12 14:46:59', 0);
 
 -- --------------------------------------------------------
 
@@ -114,6 +142,12 @@ ALTER TABLE `locations`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `research`
+--
+ALTER TABLE `research`
+  ADD PRIMARY KEY (`research_ID`);
+
+--
 -- Indexes for table `user_accounts`
 --
 ALTER TABLE `user_accounts`
@@ -135,7 +169,12 @@ ALTER TABLE `user_level`
 -- AUTO_INCREMENT for table `locations`
 --
 ALTER TABLE `locations`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+--
+-- AUTO_INCREMENT for table `research`
+--
+ALTER TABLE `research`
+  MODIFY `research_ID` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT for table `user_accounts`
 --
