3dc2e2f79d638db8c7ea2cb0248f2e55a1158a70
Rhalp Darren R. Cabrera <rhalpdarrencabrera@gmail.com>
2019-03-21 04:41:45 +0800

    account

--- dashboard/account.php
+++ dashboard/account.php
@@ -1,13 +1,13 @@
-﻿ <?php 
+﻿<?php 
     include('../session.php');
     include('dash-global-function.php');
 
    
     $pagename = "Account Management";
     $username = $_SESSION['user_Name'];
+    $script_for_specific_page = "";
     $user_img = "../assets/images/user.png";
     $user_email = "mail@gmail.com";
-    $script_for_specific_page = "";
     if(isset($_SESSION['login_level']) )
     {      
         $login_level = $_SESSION['login_level'];
@@ -28,10 +28,12 @@
 
 <!DOCTYPE html>
 <html>
+
  <?php
     include("dash-head.php");
     ?>
-<body class="theme-green">
+
+<body class="theme-green ">
     <!-- Page Loader -->
     <div class="page-loader-wrapper">
         <div class="loader">
@@ -64,61 +66,60 @@
 
     <section class="content">
         <div class="container-fluid">
-            <?php
-              echo '<pre>';
-            var_dump($_SESSION);
-            echo '</pre>';
-            ?>
-             <!-- Account Table -->
-            <div class="row clearfix">
-                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                    <div class="card">
-                        <div class="header">
-                            <h2>
-                                Account Management
-                            </h2>
-                        </div>
-                        <div class="body">
-                            <div class="table-responsive">
-                                     <div class="removeMessages"></div>
-
-                <button class="btn bg-green pull pull-right" data-toggle="modal" data-target="#addMember" id="addMemberModalBtn">
-                    <span class="glyphicon glyphicon-plus-sign"></span> Add Member
-                </button>
-                <br /> <br /> <br />
-
-                <table class="table table-bordered table-striped table-hover dataTable" id="manageMemberTable">                    
-                    <thead>
-                        <tr>
-                            <th>ID</th>
-                            <th>Level</th>                                                  
-                            <th>Username</th>
-                            <th>Status</th>                             
-                            <th>Option</th>
-                        </tr>
-                    </thead>
-                </table>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+            <div class="block-header">
+                <h2>
+                    Biogas Data Mapper
+                </h2>
             </div>
-            <!-- #END# Account Table -->
+
+            <ol class="breadcrumb breadcrumb-bg-green">
+                <li><a href="index"><i class="material-icons">home</i> Home</a></li>
+                <li  class="active"><a href="javascript:void(0);"><i class="material-icons ">account_box</i> Account Management</a></li>
+            </ol>
+            <div class="row clearfix">
+                       <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                           <div class="card">
+                               <div class="header">
+                                   <h2>LIST OF ACCOUNT</h2>
+                                   <div class="btn-group pull-right">
+                                   <button type="button" class="btn btn-success waves-effect add" data-toggle="modal" data-target="#account_modal">ADD ACCOUNT</button>
+                                   </div>
+                                   <br>
+                               </div>
+                               <div class="body">
+                                   <div class="table-responsive" style="overflow-x: hidden;">
+                                          <table id="account_data" class="table table-bordered table-striped">
+                                            <thead>
+                                              <tr>
+                                                <th width="10%">ID</th>
+                                                <th width="10%">Level</th>
+                                                <th width="10%">Username</th>
+                                                <th width="10%">Status</th>
+                                                <th width="10%">Register</th>
+                                                <th width="10%">Action</th>
+                                              </tr>
+                                            </thead>
+                                          </table>
+                                       
+                                   </div>
+                               </div>
+                           </div>
+                    </div>
+            </div>   
+          <!--    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                    <div class="card">
+                        <iframe src="map/user-map.php" style=" display:block; width:100%; height: 800px;"></iframe>
+                    </div>
+                </div> -->
+          
         </div>
+
     </section>
 
 
-    <!-- jquery plugin -->
-    <script type="text/javascript" src="../assets/jquery/jquery.min.js"></script>
-    
-    <!-- include custom index.js -->
-    <script type="text/javascript" src="../assets/js/index.js"></script>
-    <?php 
-        include("dash-js.php");
-    ?>
-</body>
-    <!-- add modal -->
-    <div class="modal fade" tabindex="-1" role="dialog" id="addMember">
+
+ <!-- add modal -->
+    <div class="modal fade" tabindex="-1" role="dialog" id="account_modal">
       <div class="modal-dialog" role="document">
         <div class="modal-content">
           <div class="modal-header">
@@ -126,18 +127,18 @@
             <h4 class="modal-title"><span class="glyphicon glyphicon-plus-sign"></span> Add Account</h4>
           </div>
           
-          <form class="form-horizontal" action="php_action/create.php" method="POST" id="createMemberForm">
+          <form class="form-horizontal" action="php_action/create.php" method="POST" id="account_form" enctype="multipart/form-data">
 
           <div class="modal-body">
             <div class="messages"></div>
               <div class="row clearfix">
                   <div class="col-lg-2 col-md-2 col-sm-4 col-xs-5 form-control-label">
-                      <label for="name">Username</label>
+                      <label for="username">Username</label>
                   </div>
                   <div class="col-lg-10 col-md-10 col-sm-8 col-xs-7">
                       <div class="form-group">
                           <div class="form-line">
-                              <input type="text" class="form-control" id="name" name="name" placeholder="Name">
+                              <input type="text" class="form-control" id="username" name="username" placeholder="username">
                           </div>
                       </div>
                   </div>
@@ -153,8 +154,8 @@
                                <select class="form-control" name="level" id="level" >
                                 <option value="">~~SELECT~~</option>
                                 <option value="1">Student</option>
-                                <option value="0">Instructor</option>
-                                <option value="2">Admin</option>
+                                <option value="2">Researcher</option>
+                                <option value="3">Admin</option>
                               </select>
                           </div>
                       </div>
@@ -181,7 +182,7 @@
                   <div class="col-lg-10 col-md-10 col-sm-8 col-xs-7">
                       <div class="form-group">
                           <div class="form-line">
-                              <input type="text" class="form-control" id="pass" name="pass" placeholder="Password">
+                              <input type="password" class="form-control" id="pass" name="pass" placeholder="Password">
                           </div>
                       </div>
                   </div>
@@ -194,7 +195,7 @@
                   <div class="col-lg-10 col-md-10 col-sm-8 col-xs-7">
                       <div class="form-group">
                           <div class="form-line">
-                              <input type="text" class="form-control" id="con_pass" name="con_pass" placeholder="Confirm Your Password">
+                              <input type="password" class="form-control" id="con_pass" name="con_pass" placeholder="Confirm Your Password">
                           </div>
                       </div>
                   </div>
@@ -220,102 +221,175 @@
 
           </div>
           <div class="modal-footer">
+          <input type="hidden" name="user_ID" id="user_ID" />
+          <input type="hidden" name="operation" id="operation" value="Add" />
+          <input type="submit" name="action" id="action" class="btn btn-success" value="Submit" />
             <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-            <button type="submit" class="btn btn-primary">Save changes</button>
           </div>
           </form> 
         </div><!-- /.modal-content -->
       </div><!-- /.modal-dialog -->
     </div><!-- /.modal -->
     <!-- /add modal -->
+    <!-- Jquery Core Js -->
+    <script src="../assets/plugins/jquery/jquery.min.js"></script>
 
-    <!-- remove modal -->
-    <div class="modal fade" tabindex="-1" role="dialog" id="removeMemberModal">
-      <div class="modal-dialog" role="document">
-        <div class="modal-content">
-          <div class="modal-header">
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-            <h4 class="modal-title"><span class="glyphicon glyphicon-trash"></span> Remove Member</h4>
-          </div>
-          <div class="modal-body">
-            <p>Do you really want to remove ?</p>
-          </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-            <button type="button" class="btn btn-primary" id="removeBtn">Save changes</button>
-          </div>
-        </div><!-- /.modal-content -->
-      </div><!-- /.modal-dialog -->
-    </div><!-- /.modal -->
-    <!-- /remove modal -->
-    <!-- edit modal -->
-    <div class="modal fade" tabindex="-1" role="dialog" id="editMemberModal">
-      <div class="modal-dialog" role="document">
-        <div class="modal-content">
-          <div class="modal-header">
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-            <h4 class="modal-title"><span class="glyphicon glyphicon-edit"></span> Edit Account</h4>
-          </div>
+    <!-- Bootstrap Core Js -->
+    <script src="../assets/plugins/bootstrap/js/bootstrap.js"></script>
 
-        <form class="form-horizontal" action="php_action/update.php" method="POST" id="updateMemberForm">         
+    <!-- Select Plugin Js -->
+    <script src="../assets/plugins/bootstrap-select/js/bootstrap-select.js"></script>
 
-          <div class="modal-body">
+    <!-- Slimscroll Plugin Js -->
+    <script src="../assets/plugins/jquery-slimscroll/jquery.slimscroll.js"></script>
 
-              <div class="edit-messages"></div>
-              <div class="row clearfix">
-                  <div class="col-lg-2 col-md-2 col-sm-4 col-xs-5 form-control-label">
-                      <label for="editName">Name</label>
-                  </div>
-                  <div class="col-lg-10 col-md-10 col-sm-8 col-xs-7">
-                      <div class="form-group">
-                          <div class="form-line">
-                              <input type="text" class="form-control" id="editName" name="editName" placeholder="">
-                          </div>
-                      </div>
-                  </div>
-              </div>
-              <br>
-               <div class="row clearfix">
-                  <div class="col-lg-2 col-md-2 col-sm-4 col-xs-5 form-control-label">
-                      <label for="editEmail">Email</label>
-                  </div>
-                  <div class="col-lg-10 col-md-10 col-sm-8 col-xs-7">
-                      <div class="form-group">
-                          <div class="form-line">
-                              <input type="text" class="form-control" id="editEmail" name="editEmail" placeholder="">
-                          </div>
-                      </div>
-                  </div>
-              </div>
-              <br>
-              <div class="row clearfix">
-                  <div class="col-lg-2 col-md-2 col-sm-4 col-xs-5 form-control-label">
-                      <label for="editStatus">Active</label>
-                  </div>
-                  <div class="col-lg-10 col-md-10 col-sm-8 col-xs-7">
-                      <div class="form-group">
-                          <div class="form-line">
-                               <select class="form-control" name="editStatus" id="editStatus" >
-                                <option value="">~~SELECT~~</option>
-                                <option value="1">Activate</option>
-                                <option value="0">Deactivate</option>
-                                <option value="2">Ban</option>
-                              </select>
-                          </div>
-                      </div>
-                  </div>
-              </div>     
-          </div>
-          <div class="modal-footer editMemberModal">
-            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-            <button type="submit" class="btn btn-primary">Save changes</button>
-          </div>
-          </form>
-        </div><!-- /.modal-content -->
-      </div><!-- /.modal-dialog -->
-    </div><!-- /.modal -->
-    <!-- /edit modal -->
+    <!-- Waves Effect Plugin Js -->
+    <script src="../assets/plugins/node-waves/waves.js"></script>
 
-    </div>
-</div>
+    <!-- Jquery DataTable Plugin Js -->
+    <script src="../assets/plugins/jquery-datatable/jquery.dataTables.js"></script>
+    <script src="../assets/plugins/jquery-datatable/skin/bootstrap/js/dataTables.bootstrap.js"></script>
+    <script src="../assets/plugins/jquery-datatable/extensions/export/dataTables.buttons.min.js"></script>
+    <script src="../assets/plugins/jquery-datatable/extensions/export/buttons.flash.min.js"></script>
+    <script src="../assets/plugins/jquery-datatable/extensions/export/jszip.min.js"></script>
+    <script src="../assets/plugins/jquery-datatable/extensions/export/pdfmake.min.js"></script>
+    <script src="../assets/plugins/jquery-datatable/extensions/export/vfs_fonts.js"></script>
+    <script src="../assets/plugins/jquery-datatable/extensions/export/buttons.html5.min.js"></script>
+    <script src="../assets/plugins/jquery-datatable/extensions/export/buttons.print.min.js"></script>
+
+    <!-- Custom Js -->
+    <script src="../assets/js/admin.js"></script>
+    <script src="../assets/js/pages/tables/jquery-datatable.js"></script>
+
+    <!-- Demo Js -->
+    <script src="../assets/js/demo.js"></script>
+    <script type="text/javascript" language="javascript" >
+$(document).ready(function(){
+
+  //select specific dropdown when updating 1 data
+  $("select[value]").each(function(){
+    $(this).val(this.getAttribute("value"));
+  });
+
+
+  var dataTable = $('#account_data').DataTable({
+    "processing":true,
+    "serverSide":true,
+    "order":[],
+    "ajax":{
+      url:"datatable/account/fetch.php",
+      type:"POST"
+    },
+    "columnDefs":[
+      {
+        "targets":[0],
+        "orderable":false,
+      },
+    ],
+
+  });
+
+  $(document).on('submit', '#account_form', function(event){
+    event.preventDefault();
+    var username = $('#username').val();
+    var level = $('#level').val();
+    var email = $('#email').val();
+    var pass = $('#pass').val();
+    var con_pass = $('#con_pass').val();
+    var status = $('#status').val();
+    if(username != '' && level != '' && email != '' && pass != '' && con_pass != '' && status != '')
+    {
+
+
+      if (pass == con_pass) 
+        {
+
+          if (pass.length > 6) {
+            $.ajax({
+              url:"datatable/account/insert.php",
+              method:'POST',
+              data:new FormData(this),
+              contentType:false,
+              processData:false,
+              success:function(data)
+              {
+                $('#action').val("Add");
+                $('#operation').val("Add");
+
+                alert(data);
+                $('#account_form')[0].reset();
+                $('#account_modal').modal('hide');
+                dataTable.ajax.reload();
+              }
+            });
+          } 
+          else {
+            alert("Minumum Password Length is 6 Character");
+          }
+        } 
+        else {
+          alert("Password not match");
+        }
+    }
+    else
+    {
+      alert("Fields are Required");
+    }
+  });
+
+  $(document).on('click', '.update', function(){
+    var user_ID = $(this).attr("id");
+    
+    $.ajax({
+      url:"datatable/account/fetch_single.php",
+      method:"POST",
+      data:{user_ID:user_ID},
+      dataType:"json",
+      success:function(data)
+      {
+        $('#account_modal').modal('show');
+        $("#username").prop("disabled", true);
+        $('#username').val(data.user_Name);
+        $('#email').val(data.user_Email);
+        $('#pass').val(data.user_Pass);
+        $('#con_pass').val(data.user_Pass);
+        $('#level').val(data.level_ID).change();
+        $('#status').val(data.user_status).change();
+        $('#action').val("Edit");
+        $('#operation').val("Edit");
+        $('.modal-title').text("Edit Account Info");
+        $('#user_ID').val(user_ID);
+      }
+    })
+  });
+  $(document).on('click', '.add', function(){
+        $("#username").prop("disabled", false);
+        document.getElementById("account_form").reset();
+  });
+  $(document).on('click', '.delete', function(){
+    var user_ID = $(this).attr("id");
+    if(confirm("Are you sure you want to delete this?"))
+    {
+      $.ajax({
+        url:"datatable/account/delete.php",
+        method:"POST",
+        data:{user_ID:user_ID},
+        success:function(data)
+        {
+          alert(data);
+          dataTable.ajax.reload();
+        }
+      });
+    }
+    else
+    {
+      return false; 
+    }
+  });
+  
+  
+});
+</script>
+</body>
+
 </html>
